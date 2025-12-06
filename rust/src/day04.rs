a27de17b05adfce7a7ea7378cab4a5c3b31837a3
resurtm <resurtm@gmail.com>
2025-12-06 06:15:07 +0100

    Day 04, part 1

---
+++ rust/src/day04.rs
@@ -0,0 +1,71 @@
+use std::fs;
+
+pub fn solve() {
+    solve_internal("test0");
+    solve_internal("google");
+    solve_internal("gh");
+}
+
+fn solve_internal(test_name: &str) -> (i32, i32) {
+    let grid: Vec<Vec<bool>> = fs::read_to_string(format!("../data/day04/{}.txt", test_name))
+        .expect("cannot read input file")
+        .split("\n")
+        .map(|x| x.trim())
+        .filter(|x| !x.is_empty())
+        .map(|x| x.chars().map(|y| y == '@').collect())
+        .fold(vec![], |mut acc, x| {
+            acc.push(x);
+            acc
+        });
+    let (w, h) = (grid[0].len(), grid.len());
+    let (mut res0, res1) = (0, 0);
+    for j in 0..h as i32 {
+        for i in 0..w as i32 {
+            if !grid[j as usize][i as usize] {
+                continue;
+            }
+            if [
+                (i - 1, j - 1),
+                (i - 1, j),
+                (i - 1, j + 1),
+                (i + 1, j - 1),
+                (i + 1, j),
+                (i + 1, j + 1),
+                (i, j - 1),
+                (i, j + 1),
+            ]
+            .into_iter()
+            .filter(|&(x, y)| x >= 0 && x < w as i32 && y >= 0 && y < h as i32)
+            .fold(0, |acc, (x, y)| {
+                if grid[y as usize][x as usize] {
+                    acc + 1
+                } else {
+                    acc
+                }
+            }) < 4
+            {
+                res0 += 1;
+            }
+        }
+    }
+    println!("--- --- --- --- ---");
+    println!("Test name: {}", test_name);
+    println!("Day 04, part 1: {}", res0);
+    println!("Day 04, part 2: {}", res1);
+    (res0, res1)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve_internal;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(solve_internal("test0").0, 13);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(solve_internal("test0").1, 0);
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     // aoc25::day01::solve();
     // aoc25::day02::solve();
-    aoc25::day03::solve();
+    // aoc25::day03::solve();
+    aoc25::day04::solve();
 }
